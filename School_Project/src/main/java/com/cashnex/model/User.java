d8e0a116d2605c7bc6ff5609a058a541819162ea
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2024-02-10 13:24:06 +0630

    - no updates for userRegistration
    - configure User class accordingly
    
    - configure Account Dao for List of Users
    
    - update pendingUserInfoDao

--- School_Project/src/main/java/com/cashnex/model/User.java
+++ School_Project/src/main/java/com/cashnex/model/User.java
@@ -1,27 +1,30 @@
 package com.cashnex.model;
+
 public class User {
 
 	private int userId;
-    private String username;
-    private String email;
-    private String password;
+	private String username;
+	private String nrcNumber;
+	private String career;
+	private String email;
+	private String password;
+	private Double balance;
 
-    public void setUserId(int userId) {
-    	this.userId = userId;
-    }
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Object getEmail() {
 		return this.email;
@@ -37,5 +40,29 @@ public class User {
 
 	public int getId() {
 		return this.userId;
+	}
+
+	public String getNrcNumber() {
+		return nrcNumber;
+	}
+
+	public void setNrcNumber(String nrcNumber) {
+		this.nrcNumber = nrcNumber;
+	}
+
+	public String getCareer() {
+		return career;
+	}
+
+	public void setCareer(String career) {
+		this.career = career;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
 	}
 }
